5cb881f3b6af300a26a5e153d2610cd7351d8f32
Mark Boszko <mboszko@mac.com>
2023-12-06 14:20:34 -0800

    Solved 03 pt1

--- 03_gear_ratios.py
+++ 03_gear_ratios.py
@@ -1,34 +1,28 @@
 import re
 
-## FIXME: My answer is too high :-/
-
 # Puzzle input from https://adventofcode.com/2023/day/3/input
 file = open("03_input.txt", "r")
-engineSchematic = file.readlines()
+input = file.readlines()
+
+# It was treating the newline character as a 'symbol',
+# So let's strip the strings
+engineSchematic = []
+for line in input:
+  engineSchematic.append(line.strip()) 
 
 sumOfPartNumbers = 0
 
 def validateChar(char):
-  print(char)
   if char.isdigit() or char == '.':
     return False
   else:
     return True
 
-for lineIndex, line in enumerate(engineSchematic):
-  # Troubleshooting
-  if lineIndex - 1 >= 0:
-    print(lineIndex - 1, engineSchematic[lineIndex - 1])
-  print(lineIndex, line)
-  if lineIndex + 1 < len(engineSchematic):
-    print(lineIndex + 1, engineSchematic[lineIndex + 1])
-  
+for lineIndex, line in enumerate(engineSchematic):  
   # charIndex will help me check for duplicate numbers on the same line
   charIndex = 0
   partNumerals = re.findall("\d+", line)
-  # do I need to check if any of these are duplicates?
   for partNumeral in partNumerals:
-    print(f">>>{partNumeral}")
     validPart = False
     # get the index of number in the string
     numeralIndex = line.find(partNumeral, charIndex)
@@ -36,13 +30,13 @@ for lineIndex, line in enumerate(engineSchematic):
       print("Didn't find numerals; something's gone wrong")
     # get how long it is
     numeralLength = len(partNumeral)
-    # print(f"partNumeral: {partNumeral}, numeralIndex: {numeralIndex}, numeralLength: {numeralLength}\n")
     
     # check for symbols around it
     #   stay within `engineSchematic` AND `line` bounds
     # How can we check?
     # - check characters one by one
     # - We could also add all of the characters to a string and then check once ... maybe this is dumb
+
     # To the left
     if numeralIndex > 0 and not validPart:
       char = line[numeralIndex - 1]
@@ -59,23 +53,20 @@ for lineIndex, line in enumerate(engineSchematic):
     # Upper
     if lineIndex > 0 and not validPart:
       checkString = engineSchematic[lineIndex - 1][lineStartIndex : lineEndIndex]
-      # print(checkString)
       for char in checkString:
         validPart = validateChar(char)
         if validPart:
           break
     # Lower
     if lineIndex < len(engineSchematic) - 1 and not validPart:
-      print()
       checkString = engineSchematic[lineIndex + 1][lineStartIndex : lineEndIndex]
-      # print(checkString)
       for char in checkString:
         validPart = validateChar(char)
         if validPart:
           break
+
     # If there is a symbol, add partNumber as int to sumOfPartNumbers
     if validPart:
-      print(f"{partNumeral} is VALID")
       sumOfPartNumbers += int(partNumeral)
     # Update `charIndex` so we only search later in the line
     charIndex = numeralIndex + numeralLength
